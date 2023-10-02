005090e5cdd9bca95ace330312fc07708dffe13d
JPMO3 <125242375+JPMO3@users.noreply.github.com>
2023-10-02 09:33:04 +0800

    Update home.php
    Added Internal CSS for home.php. (John Paul Milagrosa)

--- login/admin/home.php
+++ login/admin/home.php
@@ -20,6 +20,111 @@ if (isset($_GET['logout'])) {
 	
 	
 	<style>   
+		*{
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+        background-image: url('images/coffeebg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+          
+    }
+    .header{
+    width: 30%;
+    color: white;
+  background-color: rgba(250, 250, 250,0.1) ;
+    border-radius: 5px 10px 0px 0px;
+    padding: 20px;
+    margin-left: 30%;
+    margin-top: 15%;
+    border: 1px solid whitesmoke;
+    text-align: center;
+}
+.content {
+    width: 30%;
+    margin-left: 30%;
+    padding: 20px;
+    border: 1px solid whitesmoke;
+    background-color: rgba(120, 120, 120,0.2) ;
+    border-radius: 0px 0px 10px 10px;
+    text-align: center;
+}
+
+.btn{
+    margin-top:5%;
+    margin-left: 38%;
+padding: 15px;
+font-size: 15px;
+color: white;
+background-color:#FF597B;
+border: none;
+font-family: 'Times New Roman', Times, serif;
+width: 100px;
+display: block;
+
+}
+.btn:hover{
+
+padding: 15px;
+font-size: 15px;
+background-color:transparent;
+border: none;
+font-family: 'Times New Roman', Times, serif;
+border: 1px solid #FF597B;
+
+}
+
+.btns{
+    margin-top:5%;
+    margin-left: 38%;
+padding: 15px;
+    font-size: 15px;
+    color: white;
+  background-color:#189AB4;
+    font-family: 'Times New Roman', Times, serif;
+  width: 100px;
+    display: block;
+}
+.btns:hover{
+
+    padding: 15px;
+    font-size: 15px;
+  background-color:transparent;
+    font-family: 'Times New Roman', Times, serif;
+  border: 1px solid #189AB4;
+
+}
+
+.error {
+    width: 40%; 
+    margin: 0px auto; 
+    padding: 10px; 
+    color: White; 
+    background: transparent; 
+    border-radius: 5px; 
+    text-align: left;
+}
+.success {
+  background-color:transparent;
+    background: transparent; 
+    margin-bottom: 20px;
+}
+
+#success{
+    color:white;
+
+}
+
+.profile_info{
+    color: white;
+    font-size: 19px;
+        
+}   
+
 	</style>
 
 	
